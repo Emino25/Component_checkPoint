be5d569bc0c4f04896ac5e116102d0811b2cc876
Amin <eminohamadi25@gmail.com>
2022-03-03 12:29:13 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { ProfilePhoto } from './Components/Profile/ProfilePhoto';
+import { FullName } from './Components/Profile/FullName';
+import { Address } from './Components/Profile/Address';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePhoto/>
+      <div>
+      <FullName/>
+      <Address/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Profile/ProfilePhoto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ProfilePhoto = () => {
+  return (
+    <div>
+        <img className='profile-picture' src="/Profile.JPG" alt="" />
+    </div>
+  )
+}
